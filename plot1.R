ab5b0847172041005ebb3ad794fd641a3dafd6c9
roma-goodok <roma@goodok.ru>
2014-08-10 19:40:00 +0400

    load data from file is simplified

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 ## Note: 
-## This script was created on a Windows 7 x64, R version: R version 3.1.1 (2014-07-10)
+## This script was created on a Windows 7 x64, R version: R version 3.1.1 (2014-08-10)
 
 ## data source file stored on "Data\Coursera\exdata-004\Programm Assigment 1\data\" folder 
 ## scripts are saved in the   "Data\Coursera\exdata-004\Programm Assigment 1\ExData_Plotting1\" folder
@@ -9,24 +9,18 @@ Sys.info()["sysname"]
 Sys.info()["release"]
 
 
-setwd("C:\\Data\\Coursera\\exdata-004\\Programm Assigment 1\\ExData_Plotting1\\")
-fileName <- "..\\data\\household_power_consumption.txt"
+setwd("C:/Data/Coursera/exdata-004/Programm Assigment 1/ExData_Plotting1/")
+fileName <- "../data/household_power_consumption.txt"
 
 #
 #  Load data from file:
 #  
 
-initial <- read.table(fileName,  header = TRUE, sep=";", nrows = 1000, na.strings <- c("?"))
-classes <- sapply(initial, class)
-
 d.draft <- read.table(fileName,  header = TRUE,   sep=";", na.strings = c("?"))
 
-#
+
 # filter loaded dataset and bind row with converted to datetime  "Date", "Time" factor
-#
-
 d <- d.draft[d.draft$Date == "2/2/2007" | d.draft$Date == "1/2/2007",]
-
 d <- data.frame(d, datetime =  strptime( paste(d$Date,d$Time), "%d/%m/%Y %H:%M:%S"))
 
 names(d)
@@ -42,3 +36,4 @@ png(file = "plot1.png", width = 480, height = 480)
 hist(d$Global_active_power, main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 
 dev.off()
+
--- plot2.R
+++ plot2.R
@@ -9,24 +9,18 @@ Sys.info()["sysname"]
 Sys.info()["release"]
 
 
-setwd("C:\\Data\\Coursera\\exdata-004\\Programm Assigment 1\\ExData_Plotting1\\")
-fileName <- "..\\data\\household_power_consumption.txt"
+setwd("C:/Data/Coursera/exdata-004/Programm Assigment 1/ExData_Plotting1/")
+fileName <- "../data/household_power_consumption.txt"
 
 #
 #  Load data from file:
 #  
 
-initial <- read.table(fileName,  header = TRUE, sep=";", nrows = 1000, na.strings <- c("?"))
-classes <- sapply(initial, class)
-
 d.draft <- read.table(fileName,  header = TRUE,   sep=";", na.strings = c("?"))
 
-#
+
 # filter loaded dataset and bind row with converted to datetime  "Date", "Time" factor
-#
-
 d <- d.draft[d.draft$Date == "2/2/2007" | d.draft$Date == "1/2/2007",]
-
 d <- data.frame(d, datetime =  strptime( paste(d$Date,d$Time), "%d/%m/%Y %H:%M:%S"))
 
 names(d)
